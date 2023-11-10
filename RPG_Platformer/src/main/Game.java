1fb9d84bd78b3fe825941ad2ed82a60ab7780d36
matthieujulie07 <matthieujulie07@gmail.com>
2023-11-10 11:55:08 +0100

    Issue fix: improve game loop

--- RPG_Platformer/src/main/Game.java
+++ RPG_Platformer/src/main/Game.java
@@ -1,14 +1,86 @@
 package main;
 
-public class Game {
+import java.awt.*;
+import java.lang.management.PlatformLoggingMXBean;
+
+public class Game implements Runnable {
+    /// ------------------------------- ATTRIBUTES ------------------------------- ///
     private GameWindow window;
     private GamePanel panel;
 
+    // Game Loop and FPS settings
+    private Thread gameThread;
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
+
+
+    /// ------------------------------- CONSTRUCTOR ------------------------------- ///
     public Game() {
 
-        panel = new GamePanel();
+        panel = new GamePanel(this);
         window = new GameWindow(panel);
-
+        panel.setFocusable(true);
         panel.requestFocus();
+
+        startGameLoop();
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void update() {
+        
+    }
+
+    public void render(Graphics g) {
+        g.fillRect(100 + panel.xDelta, 100 + panel.yDelta, 200, 50);
+    }
+
+
+    @Override
+    public void run() {
+        // Set FPS and UPS
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+
+        // Var
+        long previousTime = System.nanoTime();
+
+        int frame = 0;
+        int update = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        double deltaU = 0;
+        double deltaF = 0;
+
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+
+            if (deltaU >= 1) {
+                update();
+                update++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {
+                panel.repaint();
+                frame++;
+                deltaF--;
+            }
+
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS : " + frame + " | UPS: " + update);
+                frame = 0;
+                update = 0;
+            }
+        }
     }
 }
